4391ad70492346ec1f528b96354e034d38764439
Javier <javier.martinez@mexmax-internet.com>
2021-03-16 17:08:12 -0600

    Init repo

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f10f684c795baa2db48bf4467c8737c'),
+    },
+  },
+});
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '306f50b6-836f-4229-a380-dfd7e0f4be83'
+};
